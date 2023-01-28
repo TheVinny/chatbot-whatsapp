78b53cbc3fc854240923d6206078285ec252df5b
vinny <vinny.vip07@gmail.com>
2023-01-28 20:45:56 -0300

    feat: main file app

---
+++ src/App.ts
@@ -0,0 +1,44 @@
+import { Client, LocalAuth } from 'whatsapp-web.js';
+
+import qrcode from 'qrcode-terminal';
+import 'dotenv/config';
+import { ImageToStickerService } from './services/ImageToStickerService';
+import { GetRandomDogStickerService } from './services/GetRandomDogStickerService';
+
+const client = new Client({
+  puppeteer: {
+    args: ['--no-sandbox'],
+  },
+  authStrategy: new LocalAuth(),
+});
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+
+  const number = process.env.PHONE_NUMBER;
+
+  const text = 'Bot connectado';
+
+  const chatId = number.substring(1) + '@c.us';
+
+  client.sendMessage(chatId, text);
+});
+
+client.on('message', async msg => {
+  if (msg.body == '!sticker') {
+    await ImageToStickerService(msg);
+  }
+
+  if (msg.body == '!dog') {
+    await GetRandomDogStickerService(msg);
+  }
+});
+
+client
+  .initialize()
+  .then(() => console.log('inicializou'))
+  .catch(err => console.log('error -> ' + err));
